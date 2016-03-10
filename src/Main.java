996ef8e94f46936cc74c9da2a0e4e0fc94076ff1
ROger Holten <roger.holten@gmail.com>
2016-03-10 15:13:49 +0100

    Testcommit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * Created by roger on 10.03.2016.
+ */
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Testvindu.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
