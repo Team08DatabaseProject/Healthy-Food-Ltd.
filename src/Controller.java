cfae8347085499d7967495fce3c0d18bc8704798
rogerholten <roger.holten@gmail.com>
2016-03-11 13:06:35 +0100

    Tester tabeller

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,15 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,21 +17,41 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
 	@FXML
-	public TableView tables; // Retrieves TableView with fx:id="tables"
-
-	public void initialize(URL fxmlFileLocation, ResourceBundle resources) { // Required method for Initializable, runs at program launch
-		final ObservableList<TableTest> data = FXCollections.observableArrayList(
+	//public TableView tables; // Retrieves TableView with fx:id="tables"
+	public Button ingredients;
+	public BorderPane rootPane;
+	final ObservableList<TableTest> data = FXCollections.observableArrayList(
 			new TableTest("1", "5"),
 			new TableTest("2", "2"),
 			new TableTest("3", "3"),
 			new TableTest("4", "1"),
 			new TableTest("5", "9")
-		);
+	);
 
-		ObservableList<TableColumn> columns = tables.getColumns();
-		columns.get(0).setCellValueFactory(new PropertyValueFactory<TableTest,String>("orderNo"));
-		columns.get(1).setCellValueFactory(new PropertyValueFactory<TableTest,String>("dishes"));
-		tables.setItems(data);
-		data.add(new TableTest("Fifty-three", "7")); // adds new line to table by adding another TableTest object to the data array
+	EventHandler<ActionEvent> ingButton  = new EventHandler<ActionEvent>() {
+		@Override
+		public void handle(ActionEvent e) {
+			try {
+				TableView orderTable = FXMLLoader.load(getClass().getResource("OrderTable.fxml"));
+				rootPane.setCenter(orderTable);
+				ObservableList<TableColumn> columns = orderTable.getColumns();
+				columns.get(0).setCellValueFactory(new PropertyValueFactory<TableTest,String>("orderNo"));
+				columns.get(1).setCellValueFactory(new PropertyValueFactory<TableTest,String>("dishes"));
+				orderTable.setItems(data);
+			} catch(Exception exc) {
+
+			}
+		}
+	};
+
+	public void initialize(URL fxmlFileLocation, ResourceBundle resources) { // Required method for Initializable, runs at program launch
+
+
+
+		//data.add(new TableTest("Fifty-three", "7")); // adds new line to table by adding another TableTest object to the data array
+
+		ingredients.setOnAction(ingButton);
 	}
+
+
 }
