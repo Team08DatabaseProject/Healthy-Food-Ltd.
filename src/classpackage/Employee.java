bdbb8a4e9659f68301c40d85eefd091de2dbac48
Zananok <thomas_joessund@hotmail.com>
2016-03-16 19:23:05 +0100

    Update2

--- src/classpackage/Employee.java
+++ src/classpackage/Employee.java
@@ -10,15 +10,15 @@ class Employee {
     private double salary;
     private final String passHash;
 
-    public Employee(int personId, String username, int posId, double salary, String startPassword) {
+    public Employee(int personId, String username, int posId, double salary, String passHash) {
         this.personId = personId;
         this.username = username;
         this.posId = posId;
         this.salary = salary;
 
         /* Kall til EncryptionService her for å generere passHash (og kanskje salt) */
-
-        passHash = "encryption-ting her";
+        // Thomas: If a new employee is being created (i dont meant he object.. i mean a real new one to be added to the DB), the method to encrypt the password should called before and only the hash should be passed to the object.
+        //passHash = "encryption-ting her";
     }
 
     public int getPersonId() {
