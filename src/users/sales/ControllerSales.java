5b1e0d51d307ce9ba34fa599ca27119d5ea7dc5e
axelbkv <axelbkv@stud.hist.no>
2016-04-07 14:59:59 +0200

    Lots of changes to sales

--- src/users/sales/ControllerSales.java
+++ src/users/sales/ControllerSales.java
@@ -15,6 +15,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 
 import javafx.event.ActionEvent;
+import javafx.scene.layout.GridPane;
+
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -40,7 +42,7 @@ public class ControllerSales implements Initializable {
     @FXML
     public BorderPane rootPaneSales; //RootPane
 
-    public TableView ordersTable; // Retrieves TableView with fx:id="ordersTable"
+    public GridPane ordersTable; // Retrieves TableView with fx:id="ordersTable"
     public TableView subsTable; // Retrieves Tableview with fx:id="subsTable"
 
     public Button ordersButton; //Button for showing orders
@@ -49,27 +51,23 @@ public class ControllerSales implements Initializable {
 
     private SqlQueries query = new SqlQueries();
    // final ObservableList<Order> orderTest = query.getOrders(4);
-    ZipCode zip = new ZipCode(7031, "Trondheim");
-    ZipCode zip1 = new ZipCode(7042, "Trondheim");
-
-    Address address = new Address(1, "Bugata 5", zip);
-    Address address1 = new Address(2, "Bugata 7", zip1);
-
-    LocalDate subStart = LocalDate.of(2015, 02, 11);
-    LocalDate subEnd = LocalDate.of(2016, 02, 11);
-    Subscription subscription = new Subscription(subStart, subEnd);
-
-    ObservableList<Dish> dishes = FXCollections.observableArrayList();
-    Dish dish = new Dish(20, "Ravioli");
 
 
-    final ObservableList<Customer> subsTest = FXCollections.observableArrayList(
-            new Customer(false, "arne@gmail.com", "Arne", "Knudsen", 41333183, address, "", subscription),
-            new Customer(true, "truls@gmail.com", "Truls", "Knudsen", 41333183, address1, "Business1", subscription),
-            new Customer(false, "arne@gmail.com", "Arne", "Knudsen", 41333183, address1, "", subscription),
-            new Customer(false, "arne@gmail.com", "Arne", "Knudsen", 41333183, address1, "", subscription)
-    );
+    EventHandler<ActionEvent> subsEvent = new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("SubsTable.fxml"));
+                subsTable = loader.load();
+                rootPaneSales.setCenter(subsTable);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    };
 
+    /*
     EventHandler<ActionEvent> subsEvent = new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent event) {
@@ -118,148 +116,24 @@ public class ControllerSales implements Initializable {
             }
         }
     };
+    */
 
-    final ObservableList<Customer> customerTest = FXCollections.observableArrayList(
-            new Customer(false, "arne@gmail.com", "Arne", "Knudsen", 41333183, address, "", subscription),
-            new Customer(true, "truls@gmail.com", "Truls", "Knudsen", 41333183, address1, "Business1", subscription),
-            new Customer(false, "arne@gmail.com", "Arne", "Knudsen", 41333183, address1, "", subscription),
-            new Customer(false, "arne@gmail.com", "Arne", "Knudsen", 41333183, address1, "", subscription)
-    );
 
-    LocalDate deadline = LocalDate.of(2016, 9, 11);
-    Customer customer = new Customer(true, "bæsj@poop.com", "Bæsj", "Knudsen", 26069512, address, "Rør og kloakk AS", subscription);
-    final ObservableList<Order> order = FXCollections.observableArrayList(
-            new Order(subscription, "More pickles", deadline, 367.00, "CREATED", customer, dishes),
-            new Order(subscription, "Less pickles", deadline, 367.00, "CREATED", customer, dishes),
-            new Order(subscription, "More pickles", deadline, 367.00, "CREATED", customer, dishes),
-            new Order(subscription, "More pickles", deadline, 367.00, "CREATED", customer, dishes)
-    );
 
     EventHandler<ActionEvent> orderEvent = new EventHandler<ActionEvent>() {
         @Override
-        public void handle(ActionEvent e) {
+        public void handle(ActionEvent event) {
             try {
                 FXMLLoader loader = new FXMLLoader();
                 loader.setLocation(getClass().getResource("OrdersTable.fxml"));
                 ordersTable = loader.load();
                 rootPaneSales.setCenter(ordersTable);
-                ObservableList<TableColumn> columns = ordersTable.getColumns();
-                columns.get(0).setCellValueFactory(new PropertyValueFactory<Order,Integer>("orderId"));
-                columns.get(1).setCellValueFactory(new PropertyValueFactory<Order,Customer>("customer")); //customerId
-                columns.get(2).setCellValueFactory(new PropertyValueFactory<Order,Subscription>("subscription")); //subscriptionId
-                columns.get(3).setCellValueFactory(new PropertyValueFactory<Order,Customer>("customer")); //fName
-                columns.get(4).setCellValueFactory(new PropertyValueFactory<Order,Customer>("customer")); //lName
-                columns.get(5).setCellValueFactory(new PropertyValueFactory<Order,Customer>("customer")); //businessName
-                columns.get(6).setCellValueFactory(new PropertyValueFactory<Order,Customer>("customer")); //email
-                columns.get(7).setCellValueFactory(new PropertyValueFactory<Order,Customer>("customer")); //phoneNumber
-                columns.get(8).setCellValueFactory(new PropertyValueFactory<Order,String>("customerRequests"));
-                columns.get(9).setCellValueFactory(new PropertyValueFactory<Order,LocalDate>("deadline"));
-                columns.get(10).setCellValueFactory(new PropertyValueFactory<Order,Double>("price"));
-                columns.get(11).setCellValueFactory(new PropertyValueFactory<Order,Customer>("address")); //address
-                columns.get(12).setCellValueFactory(new PropertyValueFactory<Order,String>("status"));
-                ordersTable.setItems(order);
-
-                columns.get(1).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(Integer.toString(item.getCustomerId()));
-                            }
-                        }
-                    };
-                });
-                columns.get(2).setCellFactory(column -> {
-                    return new TableCell<Order, Subscription>() {
-                        @Override
-                        protected void updateItem(Subscription item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(Integer.toString(item.getSubscriptionId()));
-                            }
-                        }
-                    };
-                });
-                columns.get(3).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(item.getFirstName());
-                            }
-                        }
-                    };
-                });
-                columns.get(4).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(item.getLastName());
-                            }
-                        }
-                    };
-                });
-                columns.get(5).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(item.getBusinessName());
-                            }
-                        }
-                    };
-                });
-                columns.get(6).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(item.getEmail());
-                            }
-                        }
-                    };
-                });
-                columns.get(7).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(Integer.toString(item.getPhoneNumber()));
-                            }
-                        }
-                    };
-                });
-                columns.get(11).setCellFactory(column -> {
-                    return new TableCell<Order, Customer>() {
-                        @Override
-                        protected void updateItem(Customer item, boolean empty) {
-                            if(item == null || empty) {
-                                setText(null);
-                            } else {
-                                setText(item.getAddress().getAddress());
-                            }
-                        }
-                    };
-                });
-            } catch(Exception exc) {
-                System.out.println("orderEvent: " + exc);
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
     };
+
 
     public ObservableList<Order> getAllOrdersForSales() {
         return allOrdersForSales;
