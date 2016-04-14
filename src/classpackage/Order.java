87417ac149a1f87cec88a45a45fed06a42d3da22
trymrt <trymrt@student.hist.no>
2016-04-14 15:47:43 +0200

    Fixed the hierarchy! GET SOME!

--- src/classpackage/Order.java
+++ src/classpackage/Order.java
@@ -1,7 +1,9 @@
 package classpackage;
 
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
 import java.time.LocalDate;
 
 /**
@@ -9,61 +11,74 @@ import java.time.LocalDate;
  */
 
 public class Order {
-    private int orderId;
-    private Customer customer;
+    private IntegerProperty orderId = new SimpleIntegerProperty();
     //private Subscription subscription; //Denne skal ikke være her på grunn av at sub tar inn mange orders. Ikke omvendt!
-    private double price;
-    private String customerRequests; //Additional info from customer
+    private DoubleProperty price = new SimpleDoubleProperty();
+    private StringProperty customerRequests = new SimpleStringProperty(); //Additional info from customer
     private LocalDate actualDeliveryDate; //Is set when order is delivered. Default null
     private LocalDate deadline; //Consists of date and time
-    private String status;
+    private StringProperty status = new SimpleStringProperty();
     private Address address;
     private ObservableList<OrderLine> dishesInThisOrder = FXCollections.observableArrayList();
 
     // From the database
-    public Order(int orderId, String customerRequests, LocalDate deadline, double price, String status) {
-        this.orderId = orderId;
-        this.customerRequests = customerRequests;
+    public Order(int orderId, String customerRequests, LocalDate deadline,
+                 LocalDate actualDeliveryDate, double price,
+                 String status, ObservableList dishesInThisOrder) {
+        this.orderId.set(orderId);
+        this.customerRequests.set(customerRequests);
         this.deadline = deadline;
-        this.price = price;
-        this.status = status;
-        actualDeliveryDate = null;
+        this.price.set(price);
+        this.status.set(status);
+        this.actualDeliveryDate = actualDeliveryDate;
+        this.dishesInThisOrder = dishesInThisOrder;
     }
 
     // To the database
-    public Order(String customerRequests, LocalDate deadline,
-                 double price, String status, Customer customer, ObservableList dishesInThisOrder) {
-        this.customerRequests = customerRequests;
+    public Order(String customerRequests, LocalDate deadline, double price,
+                 String status, ObservableList dishesInThisOrder) {
+        this.customerRequests.set(customerRequests);
         this.deadline = deadline;
-        this.price = price;
-        this.status = status;
+        this.price.set(price);
+        this.status.set(status);
         actualDeliveryDate = null;
-        this.customer = customer;
         this.dishesInThisOrder = dishesInThisOrder;
     }
 
     public int getOrderId() {
+        return orderId.get();
+    }
+
+    public IntegerProperty orderIdProperty() {
         return orderId;
     }
 
     public void setOrderId(int orderId) {
-        this.orderId = orderId;
+        this.orderId.set(orderId);
+    }
+
+    public double getPrice() {
+        return price.get();
+    }
+
+    public DoubleProperty priceProperty() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price.set(price);
     }
 
     public String getCustomerRequests() {
+        return customerRequests.get();
+    }
+
+    public StringProperty customerRequestsProperty() {
         return customerRequests;
     }
 
     public void setCustomerRequests(String customerRequests) {
-        this.customerRequests = customerRequests;
-    }
-
-    public LocalDate getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDate deadline) {
-        this.deadline = deadline;
+        this.customerRequests.set(customerRequests);
     }
 
     public LocalDate getActualDeliveryDate() {
@@ -74,20 +89,24 @@ public class Order {
         this.actualDeliveryDate = actualDeliveryDate;
     }
 
-    public double getPrice() {
-        return price;
+    public LocalDate getDeadline() {
+        return deadline;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
     }
 
     public String getStatus() {
+        return status.get();
+    }
+
+    public StringProperty statusProperty() {
         return status;
     }
 
     public void setStatus(String status) {
-        this.status = status;
+        this.status.set(status);
     }
 
     public Address getAddress() {
@@ -96,14 +115,6 @@ public class Order {
 
     public void setAddress(Address address) {
         this.address = address;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
     }
 
     public ObservableList<OrderLine> getDishesInThisOrder() {
--- src/classpackage/Subscription.java
+++ src/classpackage/Subscription.java
@@ -1,5 +1,6 @@
 package classpackage;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -9,31 +10,39 @@ import java.time.LocalDate;
  * Created by paul thomas on 17.03.2016.
  */
 public class Subscription {
-    private int subscriptionId;
+    private IntegerProperty subscriptionId;
     //LocalDate.of(2012, Month.DECEMBER, 12); // from values
     private LocalDate startSubscription;
     private LocalDate endSubscription;
-    private ObservableList<OrderLine> ordersOnThisSubscription = FXCollections.observableArrayList();
+    private ObservableList<Order> ordersOnThisSubscription = FXCollections.observableArrayList();
 
-
-    public Subscription(int subscriptionId, LocalDate startSubscription, LocalDate endSubscription, ObservableList ordersOnThisSubscription) {
-        this.subscriptionId = subscriptionId;
+    // From database
+    public Subscription(int subscriptionId, LocalDate startSubscription, LocalDate endSubscription,
+                        ObservableList ordersOnThisSubscription) {
+        this.subscriptionId.set(subscriptionId);
         this.startSubscription = startSubscription;
         this.endSubscription = endSubscription;
         this.ordersOnThisSubscription = ordersOnThisSubscription;
     }
-    public Subscription(LocalDate startSubscription, LocalDate endSubscription, ObservableList ordersOnThisSubscription) {
+
+    // To database
+    public Subscription(LocalDate startSubscription, LocalDate endSubscription,
+                        ObservableList ordersOnThisSubscription) {
         this.startSubscription = startSubscription;
         this.endSubscription = endSubscription;
         this.ordersOnThisSubscription = ordersOnThisSubscription;
     }
 
     public int getSubscriptionId() {
+        return subscriptionId.get();
+    }
+
+    public IntegerProperty subscriptionIdProperty() {
         return subscriptionId;
     }
 
     public void setSubscriptionId(int subscriptionId) {
-        this.subscriptionId = subscriptionId;
+        this.subscriptionId.set(subscriptionId);
     }
 
     public LocalDate getStartSubscription() {
@@ -52,11 +61,12 @@ public class Subscription {
         this.endSubscription = endSubscription;
     }
 
-    public ObservableList getOrdersOnThisSubscription() {
+    public ObservableList<Order> getOrdersOnThisSubscription() {
         return ordersOnThisSubscription;
     }
 
-    public void setOrdersOnThisSubscription(ObservableList ordersOnThisSubscription) {
+    public void setOrdersOnThisSubscription(ObservableList<Order> ordersOnThisSubscription) {
         this.ordersOnThisSubscription = ordersOnThisSubscription;
     }
+
 }
