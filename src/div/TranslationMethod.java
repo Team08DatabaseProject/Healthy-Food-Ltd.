d2e681808d22bb35f6d6aeec988aaf8423072cad
axelbkv <axelbkv@stud.hist.no>
2016-04-15 14:06:13 +0200

    Class with public static test objects which we can use for testing, rather than creating new ones ourselves. Also made TranslationMethod.java, which is used for creating OrderLine objects from MenuLine objects (this is necessary)

---
+++ src/div/TranslationMethod.java
@@ -0,0 +1,31 @@
+package div;
+
+import classpackage.MenuLine;
+import classpackage.OrderLine;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Created by axelkvistad on 4/15/16.
+ */
+public class TranslationMethod {
+
+    public ObservableList<OrderLine> getOrderListFromMenuList(ObservableList<MenuLine> menuLines) {
+        OrderLine orderLine;
+        ObservableList<OrderLine> orderLineList = FXCollections.observableArrayList();
+        for (MenuLine m : menuLines) {
+            orderLine = new OrderLine(m.getDish(), m.getAmount());
+            orderLineList.add(orderLine);
+        }
+        return orderLineList;
+    }
+
+    public double getOrderPriceFromMenuList(ObservableList<MenuLine> menuLines) {
+        double price = 0;
+        for (MenuLine m : menuLines) {
+            price += (m.getDish().getPrice() * m.getPriceFactor());
+        }
+        return price;
+    }
+
+}
