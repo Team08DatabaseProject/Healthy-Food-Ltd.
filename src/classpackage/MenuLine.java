f49a81cdc4ae809916f1d13b8923616046803d1e
axelbkv <axelbkv@stud.hist.no>
2016-04-24 15:43:50 +0200

    menuline

--- src/classpackage/MenuLine.java
+++ src/classpackage/MenuLine.java
@@ -1,5 +1,6 @@
 package classpackage;
 
+import javafx.beans.binding.NumberBinding;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,23 +12,27 @@ public class MenuLine {
     private ObjectProperty<Dish> dish = new SimpleObjectProperty<>();
     private IntegerProperty amount = new SimpleIntegerProperty();
     private DoubleProperty priceFactor = new SimpleDoubleProperty();
+    private NumberBinding totalPrice;   // dish price * dish amount * menuLine price factor
 
     public MenuLine(Dish dish, int amount, double priceFactor) {
         this.dish.set(dish);
         this.amount.set(amount);
         this.priceFactor.set(priceFactor);
+        totalPrice = this.amount.multiply(this.getDish().getPrice()).multiply(this.priceFactor);
     }
 
     public MenuLine(Dish dish, int amount) {
         this.dish.set(dish);
         this.amount.set(amount);
         this.priceFactor.set(1);
+        totalPrice = this.amount.multiply(this.getDish().getPrice()).multiply(priceFactor);
     }
 
     public MenuLine(Dish dish) {
         this.dish.set(dish);
         this.amount.set(1);
         this.priceFactor.set(1);
+        totalPrice = this.amount.multiply(this.getDish().getPrice()).multiply(priceFactor);
     }
 
     public Dish getDish() {
@@ -64,6 +69,10 @@ public class MenuLine {
 
     public void setPriceFactor(double priceFactor) {
         this.priceFactor.set(priceFactor);
+    }
+
+    public double getTotalPrice() {
+        return totalPrice.doubleValue();
     }
 
     // TODO: 10.04.2016 Probably needs revision, due to GUI representation
